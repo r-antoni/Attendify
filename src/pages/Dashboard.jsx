ba6a68a31d7c08cc8cbeb5847f05d574363d7701
r-antoni <reindraantoni@gmail.com>
2023-07-07 01:37:42 +0700

    .

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,52 +1,8 @@
 import attendanceReports from "../assets/json/attendanceReports.json"
+import dashboardData from "../assets/json/dashboardData.json"
 import { Breadcrumb } from "flowbite-react";
 import { Card } from "flowbite-react";
 import { PolarAngleAxis, PolarRadiusAxis, PolarGrid, Radar, RadarChart, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
 const Dashboard = () => {
   return (
@@ -118,7 +74,7 @@ const Dashboard = () => {
                   <LineChart
                     width={500}
                     height={300}
-                    data={data}
+                    data={dashboardData}
                     margin={{
                       top: 5,
                       right: 30,
