19df9669a3ef70308a9c9aeac53a2de3b6e08078
Gilbransyah12 <gilbransyah12@gmail.com>
2023-07-22 23:01:51 +0800

    .

--- src/pages/AccessDenied.jsx
+++ src/pages/AccessDenied.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Lottie from "lottie-react";
+import forbiddenAnimation from "../assets/animation/403forbidden.json"
 
 const AccessDenied = () => {
-  return <div>AccessDenied</div>;
+  return (
+    <div>
+
+      <div className="text-sm">
+        <Lottie animationData={forbiddenAnimation} />
+      </div>
+    </div>
+  )
 };
 
 export default AccessDenied;
