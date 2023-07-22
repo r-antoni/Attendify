77e3020e82ba6c53c7b4815e6948c423e293d1e2
iqbal herdiansyah <indiqbal4@gmail.com>
2023-07-22 15:44:47 +0700

    add  react-prop-type

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -2,12 +2,18 @@ import Auth from "../utils/Auth";
 import {Navigate, Outlet} from "react-router-dom";
 import Cookies from "js-cookie";
 import AccessDenied from "../pages/AccessDenied";
+import propTypes from "prop-types";
 
 export default function PrivateRoute({roleRequired}) {
   const role = Cookies.get("role");
+
   if (roleRequired) {
-    return Auth.authenticated() ? roleRequired === role ? <Outlet /> : <AccessDenied /> : <Navigate to="/" />;
+    return Auth.authenticated() ? roleRequired.includes(role) ? <Outlet /> : <AccessDenied /> : <Navigate to="/" />;
   } else {
     return Auth.authenticated() ? <Outlet /> : <Navigate to="/" />;
   }
 }
+
+PrivateRoute.propTypes = {
+  roleRequired: propTypes.array,
+};
