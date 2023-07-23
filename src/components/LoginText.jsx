d16389b4fc01e5680afb719c5f914e80b3c6eb7d
r-antoni <reindraantoni@gmail.com>
2023-07-23 11:44:33 +0700

    .

--- src/components/LoginText.jsx
+++ src/components/LoginText.jsx
@@ -2,10 +2,10 @@ const LoginText = () => {
   return (
     <div className="flex justify-center min-h-screen items-center">
       <div className="flex-row mx-auto w-1/2">
-        <h1 className="text-[#212529] font-normal text-[64px] leading-[76px]">Attendance</h1>
-        <h1 className="text-[#012970] font-normal text-[64px] leading-[76px]">for your business</h1>
+        <h1 className="text-[#212529] font-normal text-[64px] leading-[76px]">Attendance Management</h1>
+        <h1 className="text-[#012970] font-normal text-[64px] leading-[76px]">for your needs</h1>
         <p className="mt-4 text-[##757F8E]">
-          attendify is an attendance management app with role-based authorization, and also has role-based
+          Attendance management app with role-based authorization, and also has role-based
           functionality.
         </p>
       </div>
