8118812764acc9a91a7d4c4687245567a8f6e4ac
jannskuyyy <putrajanuardsetiawan@gmail.com>
2023-07-23 09:24:56 +0700

    edit text login page

--- src/components/LoginText.jsx
+++ src/components/LoginText.jsx
@@ -5,8 +5,8 @@ const LoginText = () => {
         <h1 className="text-[#212529] font-normal text-[64px] leading-[76px]">Attendance</h1>
         <h1 className="text-[#012970] font-normal text-[64px] leading-[76px]">for your business</h1>
         <p className="mt-4 text-[##757F8E]">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem ipsum beatae nam laborum dignissimos.
-          Perspiciatis quaerat ducimus ipsa assumenda maiores.
+          attendify is an attendance management app with role-based authorization, and also has role-based
+          functionality.
         </p>
       </div>
     </div>
